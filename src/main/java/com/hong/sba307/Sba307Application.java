498ffd7d27ca257638ca8ac37b4892f8bd132148
Wei Hang Hong <weihanghong2001@gmail.com>
2024-06-22 15:18:09 -0400

    init

---
+++ src/main/java/com/hong/sba307/Sba307Application.java
@@ -0,0 +1,13 @@
+package com.hong.sba307;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sba307Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sba307Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hong/sba307/Sba307ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.sba307;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sba307ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
